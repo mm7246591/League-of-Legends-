4ca5b9ffb4255447d9e35d9634538ce44e8f0e1b
Yan <mm7246591@yahoo.com.tw>
2022-02-12 14:37:35 +0800

    2/12

--- src/components/Championdetail.vue
+++ src/components/Championdetail.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="content" v-for="champion of champions" :key="champion.name">
-    <div class="img">
+    <div
+      class="img"
+      :style="{
+        backgroundImage: `url(${champion.bigImg})`,
+      }"
+    >
       <img :src="champion.bigImg" alt="" />
     </div>
     <div class="origin">{{ champion.origin }}</div>
@@ -31,10 +36,48 @@ export default {
 <style scoped>
 .content {
   width: 100%;
-  height: 500px;
+  height: 800px;
   display: flex;
   flex-direction: column;
   align-items: center;
   margin: auto;
+  background-color: black;
+  color: white;
+}
+.content .img {
+  width: 100%;
+  height: 700px;
+  position: relative;
+}
+.content .img::before {
+  content: "";
+  background: linear-gradient(black 5%, transparent 10%) center center,
+    linear-gradient(to top, black 2%, transparent 15%) center center;
+  position: absolute;
+  width: 100%;
+  top: 0;
+  height: 500px;
+}
+.content .img::after {
+  content: "";
+  width: 100%;
+  height: 99%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: inherit;
+  margin: auto;
+  filter: blur(5px);
+  z-index: 2;
+}
+.img img {
+  width: 50%;
+  height: 600px;
+  position: absolute;
+  top: 10%;
+  left: 25%;
+  z-index: 3;
 }
 </style>
